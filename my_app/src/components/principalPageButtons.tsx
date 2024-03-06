76791aca8ba5e01103db97e4618e1cc10cb995af
PedroAC18 <pedro.correa1101@gmail.com>
2024-03-05 21:18:24 -0300

    Refactor modules

--- my_app/src/components/principalPageButtons.tsx
+++ my_app/src/components/principalPageButtons.tsx
@@ -3,20 +3,13 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import PaidIcon from '@mui/icons-material/Paid';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
-import {Routes, Route, useNavigate} from 'react-router-dom';
 
 
 function PrincipalButtons(){
 
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/outgoingModule/outgoingPage.tsx');
-    }
-
     return(
         <Stack spacing={2} direction="row">
-            <Button variant = "contained" startIcon={<PaidIcon/> } onClick={handleClick}> Gastos </Button>
+            <Button variant = "contained" startIcon={<PaidIcon/>}> Gastos </Button>
             <Button variant = "contained" startIcon={<ShowChartIcon/>}> Investimentos </Button>
         </Stack>
     )
